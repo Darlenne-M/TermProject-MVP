73faa84c74bb1cbb13fa3e8ab1a9b202f4ee2313
Darlenne Noemi Mosqueda <dnmosquedaa@uncg.edu>
2026-04-16 18:25:02 -0400

    updated server.js

--- express-backend/server.js
+++ express-backend/server.js
@@ -9,8 +9,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 const recipeRoutes = require('./routes/recipeRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/recipes', recipeRoutes);
+app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
