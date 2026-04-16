3ac308b45b5553ea3796922793110cc8538f2747
Darlenne Noemi Mosqueda <dnmosquedaa@uncg.edu>
2026-04-16 18:26:45 -0400

    ..

--- express-backend/routes/userRoutes.js
+++ express-backend/routes/userRoutes.js
@@ -2,8 +2,9 @@
 const userController = require('../controllers/userController');
 const express = require("express");
 const router = express.Router();
-router.get("/", userController.fetchAllUsers);
+
 router.get("/:id", userController.fetchUserById);
+router.get("/", userController.fetchAllUsers);
 router.delete("/:id", userController.removeUser);
 router.post("/", userController.createUser);
 module.exports = router;
